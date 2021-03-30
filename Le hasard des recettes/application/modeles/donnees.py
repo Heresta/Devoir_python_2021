6130aac0f53ef377c3fc2822ef13b133aa67210a
Heresta <74970052+Heresta@users.noreply.github.com>
2021-03-30 17:55:03 +0200

    Commit initial donnees.py

---
+++ Le hasard des recettes/application/modeles/donnees.py
@@ -0,0 +1,57 @@
+from flask import url_for
+import datetime
+
+from .. app import db
+
+
+class Authorship(db.Model):
+    __tablename__ = "authorship"
+    authorship_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
+    authorship_plat_id = db.Column(db.Integer, db.ForeignKey('plat.plat_id'))
+    authorship_user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    authorship_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    user = db.relationship('User', back_populates="authorships")
+    plat = db.relationship('Plat', back_populates="authorships")
+
+    def author_to_json(self):
+        return {
+            "author": self.user.to_jsonapi_dict(),
+            "on": self.authorship_date
+        }
+
+
+class Plat(db.Model):
+    plat_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    plat_nom = db.Column(db.Text)
+    plat_recette = db.Column(db.VARCHAR(100))
+    plat_type = db.Column(db.String(45))
+    plat_nombre_convives = db.Column(db.Integer)
+    authorships = db.relationship("Authorship", back_populates="plat")
+
+    def to_jsonapi_dict(self):
+        return {
+            "type": "place",
+            "id": self.plat_id,
+            "attributes": {
+                "name": self.plat_nom,
+                "type": self.plat_type,
+                "nombre_convives": self.plat_nombre_convives,
+                "lien_recette": self.plat_recette
+            },
+            "links": {
+                "self": url_for("plat", plat_id=self.plat_id, _external=True),
+                "json": url_for("api_plats_single", plat_id=self.plat_id, _external=True)
+            },
+            "relationships": {
+                "editions": [
+                    author.author_to_json()
+                    for author in self.authorships
+                ]
+            }
+        }
+
+
+class Ingredient(db.Model):
+    ingredient_id = db.Column(db.Integer, unique=True, primary_key=True, autoincrement=True, nullable=False)
+    ingredient_nom = db.Column(db.Text)
+    ingredient_type = db.Column(db.Text)
