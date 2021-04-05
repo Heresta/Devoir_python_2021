cc5ff57668bde524026d51a7eb8fa9fcd320e275
Heresta <74970052+Heresta@users.noreply.github.com>
2021-04-05 09:37:52 +0200

    MAJ donnees.py
    Ajout de la classe Composition
    Ajout de la fonctionnalité ajout de recette
    Ajout de la fonctionnalité retrait de recette

--- Le hasard des recettes/application/modeles/donnees.py
+++ Le hasard des recettes/application/modeles/donnees.py
@@ -20,6 +20,16 @@ class Authorship(db.Model):
         }
 
 
+class Composition(db.Model):
+    __tablename__ = "composition"
+    composition_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
+    composition_plat_id = db.Column(db.Integer, db.ForeignKey('plat.plat_id'))
+    composition_ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.ingredient_id'))
+    composition_ingredient = db.relationship('Ingredient', back_populates="composition")
+    composition_plat = db.relationship('Plat', back_populates="composition")
+    quantite = db.Column(db.String)
+
+
 class Plat(db.Model):
     plat_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
     plat_nom = db.Column(db.Text)
@@ -50,6 +60,46 @@ class Plat(db.Model):
             }
         }
 
+    
+    @staticmethod
+    def ajout(nom, recette, typologie, nombre):
+        erreurs = []
+        if not nom:
+            erreurs.append("Le nom de la recette est manquant")
+        if not recette:
+            erreurs.append("Le lien vers la recette est manquant")
+        if not typologie:
+            erreurs.append("Le type de la recette est manquant")
+        if not nombre:
+            erreurs.append("L'indication du nombre de convives est manquant")
+
+        uniques = Plat.query.filter(db.or_(Plat.plat_recette == recette, Plat.plat_nom == nom)).count()
+        if uniques > 0:
+            erreurs.append("Le lien vers cette recette ou cette recette est déjà inscrit dans "
+                           "notre base de données.")
+
+        if len(erreurs) > 0:
+            return False, erreurs
+
+        plat = Plat(
+            plat_nom=nom,
+            plat_recette=recette,
+            plat_type=typologie,
+            plat_nombre_convives=nombre
+        )
+
+        try:
+            db.session.add(plat)
+            db.session.commit()
+            return True, plat
+        except Exception as erreur:
+            return False, [str(erreur)]
+        
+        @staticmethod
+    def retrait(objet):
+        db.session.delete(objet)
+        db.session.commit()
+       
 
 class Ingredient(db.Model):
     ingredient_id = db.Column(db.Integer, unique=True, primary_key=True, autoincrement=True, nullable=False)
